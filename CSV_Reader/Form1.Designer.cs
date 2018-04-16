4e96b0bb42ff76a69c64450b575771709e48aee2
Michael Pichugin <michaelpichugin@yandex.ru>
2018-04-16 23:57:47 +0700

    Complete.

--- CSV_Reader/Form1.Designer.cs
+++ CSV_Reader/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSV_Reader
 {
-    partial class Form1
+    partial class frmHome
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            this.txtCout = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCout
+            // 
+            this.txtCout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCout.Location = new System.Drawing.Point(0, 41);
+            this.txtCout.Multiline = true;
+            this.txtCout.Name = "txtCout";
+            this.txtCout.ReadOnly = true;
+            this.txtCout.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCout.Size = new System.Drawing.Size(434, 270);
+            this.txtCout.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Image = global::CSV_Reader.Properties.Resources.icons8_save_as_16;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(308, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить файл";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Image = global::CSV_Reader.Properties.Resources.icons8_open_16;
+            this.btnOpen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(107, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Открыть файл";
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCout);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(450, 350);
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSV Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtCout;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- CSV_Reader/Form1.cs
+++ CSV_Reader/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,71 @@ using System.Windows.Forms;
 
 namespace CSV_Reader
 {
-    public partial class Form1 : Form
+    public partial class frmHome : Form
     {
-        public Form1()
+        public frmHome()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            txtCout.Clear();
+            String[] arrOut;
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "CSV Files|*.csv";
+            open.FilterIndex = 1;
+            if (DialogResult.OK == open.ShowDialog())
+            {
+                try
+                {
+                    using (TextFieldParser tfp = new TextFieldParser(open.FileName))
+                    {
+                        tfp.TextFieldType = FieldType.Delimited;
+                        tfp.SetDelimiters(";");
+
+                        while (!tfp.EndOfData)
+                        {
+                            arrOut = tfp.ReadFields();
+
+                            int countColumn = 0;
+                            foreach (String data in arrOut)
+                            {
+                                txtCout.Text += data;
+                                if (countColumn < arrOut.Length - 1)
+                                {
+                                    countColumn++;
+                                    txtCout.Text += ";";
+                                }
+                                else
+                                    txtCout.Text += "\r\n";
+                            }
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV Files|*.csv";
+            sfd.FilterIndex = 1;
+            if(DialogResult.OK == sfd.ShowDialog() && txtCout.Text != "")
+            {
+                try
+                {
+                    File.WriteAllText(sfd.FileName, txtCout.Text);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
